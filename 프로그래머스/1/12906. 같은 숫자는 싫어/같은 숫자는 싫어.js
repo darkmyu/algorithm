839af27f1cb7ff33b47405b159c63e549240e128
darkmyu <60710735+darkmyu@users.noreply.github.com>
2025-01-19 00:26:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.87 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,19 @@
+// function solution(arr)
+// {
+//     const answer = [];
+    
+//     arr.reduce((acc, cur) => {
+//         if (acc !== cur) {
+//             answer.push(cur);
+//         }
+//         return cur;
+//     }, -1);
+    
+//     return answer;
+// }
+
+// 다른 풀이
 function solution(arr)
 {
-    const answer = [];
-    
-    arr.reduce((acc, cur) => {
-        if (acc !== cur) {
-            answer.push(cur);
-        }
-        return cur;
-    }, -1);
-    
-    return answer;
+    return arr.filter((value, index) => value !== arr[index + 1]);
 }
