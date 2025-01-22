e13487f5124d034c300fbf66520f31a0980ba5de
darkmyu <60710735+darkmyu@users.noreply.github.com>
2025-01-23 05:03:51 +0900

    [Bronze IV] Title: $A + B - C$, Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split('\n').map(Number);
+
+console.log(input[0] + input[1] - input[2]);
+console.log((input[0].toString() + input[1].toString()) - input[2]);
